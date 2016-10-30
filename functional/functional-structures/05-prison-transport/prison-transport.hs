40f5fc8bfb1a46d30cc9e8c104e9aff4495f76b6
Montycarlo <joshua.brown@au.ey.com>
2016-10-30 15:59:55 +1100

    better Prison-transport still no dice on test case #9

--- functional/functional-structures/05-prison-transport/prison-transport.hs
+++ functional/functional-structures/05-prison-transport/prison-transport.hs
@@ -1,53 +1,83 @@
-import Data.Set (Set, singleton, union, member, size, fromList, delete, insert)
+import Data.Set (Set, singleton, union, member, size, fromList, delete, insert, empty)
+import qualified Data.Map.Strict as Map
 
-type Prison = Set Int
-data Tree a = Tree a Bool (Tree a) (Tree a) | Leaf deriving Show
+data Prisoner = Prisoner{ prisonerId :: Int, truck :: Maybe Int } deriving Show
+instance Eq Prisoner where
+	x == y = (prisonerId x) == (prisonerId y)
+instance Ord Prisoner where
+	compare p1 p2 = compare (id p1) (id p2)
+
+data Truck = Truck{ truckId :: Int, prisoners :: [Prisoner] } deriving Show
+instance Eq Truck where
+	x == y = (truckId x) == (truckId y)
+instance Ord Truck where
+	compare p1 p2 = compare (truckId p1) (truckId p2)
+
+type Prison = Map.Map Int Prisoner
 
 main = (readLn :: IO Int) >>= \n -> (readLn :: IO Int) >>= ferry n
 
 ferry n m = do
 	lnks <- mapM (\_->getLine) [1..m]
-	let prison = fromList [1..n]
-	let grps = rdGrps prison $ map words lnks
-	let singles = (n - (sum $ map size grps))
-	putStrLn . show $ (+) singles $ sum $ map cost grps
+	let 
+		prison = initPrison n
+		trucks = Map.elems $ rdGrps prison $ map (\x->map read (words x)) lnks
+		singles = n - (sum $ map (\t-> length $ prisoners t) trucks)
+	putStrLn . show $ (+) singles $ sum $ map (\t-> cost $ prisoners t) trucks
 
-cost :: Set Int -> Int
-cost g = ceiling . sqrt . fromIntegral $ size g
 
-rdGrps :: Prison -> [[String]] -> [Set Int]
-rdGrps p xs = rdGrps' p xs []
-rdGrps' :: Prison -> [[String]] -> [Set Int] -> [Set Int]
-rdGrps' _ [] ys = ys
-rdGrps' p (x:xs) ys = 
-	let (p2,ys2) = mrg p (map (\r->read r :: Int) x) ys in
-	rdGrps' p2 xs ys2
+initPrison n = Map.fromList $ zip [1..n] $ map (\x -> Prisoner { prisonerId=x, truck=Nothing }) [1..n]
 
-mrg :: Prison -> [Int] -> [Set Int] -> (Prison, [Set Int])
-mrg p [a,b] ys
-	| p_a && p_b = 
-			let p2 = delete b $ delete a p in
-			(p2, (fromList [a,b]):ys)
-	| p_a = 
-			let p2 = delete a p in
-			let (truck,trucks) = pull b ys in
-			(p2, (insert a truck):trucks)
-	| p_b = 
-			let p2 = delete b p in
-			let (truck,trucks) = pull a ys in
-			(p2, (insert b truck):trucks)
-	| otherwise = 
-			let (a_truck, a_trucks) = pull a ys in
-			let (b_truck, ab_trucks) = pull b a_trucks in
-			(p, (union a_truck b_truck):ab_trucks)
-	where 
-		p_a = member a p
-		p_b = member b p
+cost :: [Prisoner] -> Int
+cost g = ceiling . sqrt . fromIntegral $ length g
 
-pull :: Int -> [Set Int] -> (Set Int, [Set Int])
-pull x [] = (singleton x, [])
-pull x (y:ys)
-	| member x y = (y, ys)
-	| otherwise = 
-		let (ret,rest) = pull x ys in
-		(ret,y:rest)
+rdGrps :: Prison -> [[Int]] -> Map.Map Int Truck
+rdGrps p xs = rdGrps' p xs Map.empty 1
+
+rdGrps' :: Prison -> [[Int]] -> Map.Map Int Truck -> Int -> Map.Map Int Truck
+rdGrps' _ [] ts _ = ts
+rdGrps' p (x:xs) ts tid =
+	let
+		(p1, p2) = extr p x
+		mgts@(truck1, truck2) = (getTruck ts p1, getTruck ts p2) in
+	case mgts of
+		(Nothing, Nothing) ->
+				let 
+					(p', ps') = updateTruckIds tid p [p1,p2]
+					newt = Truck { truckId = tid, prisoners=ps' }
+				in
+					rdGrps' p' xs (Map.insert tid newt ts) (tid+1)
+		(Just l, Nothing) ->
+				let
+					(p', [p2']) = updateTruckIds (truckId l) p [p2]
+					newt = Truck { truckId=truckId l , prisoners=(p2':prisoners l) } 
+				in
+					rdGrps' p' xs (Map.insert (truckId l) newt ts) tid
+		(Nothing, Just r) ->
+				let
+					(p', [p1']) = updateTruckIds (truckId r) p [p1]
+					newt = Truck { truckId=truckId r , prisoners=(p1':prisoners r) }
+				in
+					rdGrps' p' xs (Map.insert (truckId r) newt ts) tid
+		(Just l, Just r) ->
+				if l == r then
+					rdGrps' p xs ts tid
+				else 
+					let
+						tid' = truckId l
+						(p', pts') = updateTruckIds tid' p (prisoners r)
+						newt = Truck { truckId=truckId l , prisoners=(pts' ++ prisoners l) }
+					in
+						rdGrps' p' xs (Map.insert tid' newt (Map.delete (truckId r) ts)) tid
+
+extr p [p1,p2] = (p Map.! p1, p Map.! p2)
+getTruck :: Map.Map Int Truck -> Prisoner -> Maybe Truck
+getTruck ts p = truck p >>= (\x -> return $ ts Map.! x)
+
+updateTruckIds _ prisoners [] = (prisoners, [])
+updateTruckIds tid prisoners (p:ps) =
+	let 
+		p' = Prisoner{prisonerId=prisonerId p, truck=Just tid}
+		(prisoners', rest) = updateTruckIds tid (Map.insert (prisonerId p) p' prisoners) ps
+	in
+	(prisoners', p':rest)
